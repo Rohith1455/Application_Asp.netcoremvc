355ae464c6e84bb461a21fa80cf63b341e53cd6e
RohithR <rohith.r6@cognizant.com>
2022-07-15 22:19:55 +0530

    added basic features

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JokeApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
